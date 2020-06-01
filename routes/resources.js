27337de73121ee4f5ca0e56d4e1ca2a893b524c3
Shadid12 <Shadid12@users.noreply.github.com>
2020-06-01 03:52:47 -0400

    file upload done

--- routes/resources.js
+++ routes/resources.js
@@ -7,12 +7,27 @@ const {
   deleteResource
 } = require('../controllers/resources');
 
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function (req, file, cb) {
+    req.body.body = Date.now() + '-' + file.originalname;
+    cb(null, Date.now() + '-' + file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage });
+//
+
 const router = express.Router();
 
 router
   .route('/')
   .get(getResources)
-  .post(createResource);
+  .post(upload.single('resourceFile'),createResource);
 
 router
   .route('/:id')
